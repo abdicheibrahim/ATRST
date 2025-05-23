ebcfadfb4648508d5d20e5d0cb1369693861909e
abdicheibrahim <abdicheibrahim@gmail.com>
2025-05-23 22:12:37 +0100

    ConvertFromTPHtoTPT

---
+++ ProjetAtrst/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace ProjetAtrst.Models
+
+{
+    public class ApplicationUser: IdentityUser
+
+    {
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+    }
+}
